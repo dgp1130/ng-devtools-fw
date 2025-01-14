368011baeb62c9b0a0697ea5b3cc01ea21fb8865
Doug Parker <dgp1130@users.noreply.github.com>
2025-01-13 17:06:07 -0800

    Updates `getDirectiveMetadata` to return `props` and `state` as well as list `effects`.

--- src/ng.ts
+++ src/ng.ts
@@ -1,3 +1,5 @@
+import { EffectHandle } from './effect.js';
+
 // DevTools checks for `__ngContext__` of the root element to ensure it's running Angular Ivy.
 (document.querySelector('body > *')! as any).__ngContext__ = {};
 
@@ -19,10 +21,31 @@ export const ng = {
     // Note that Angular components *are* directives, so this is called.
     // Seems like we could use this to disambiguate properties and inputs, but
     // not entirely sure how it works right now.
-    getDirectiveMetadata(): DirectiveMetadata {
+    getDirectiveMetadata(el: Element): ComponentDebugMetadata | DirectiveDebugMetadata | null {
+        const comp = ng.getComponent(el);
+        if (!comp) return null;
+
+        const properties = Object.entries(Object.getOwnPropertyDescriptors(el));
+
         return {
             inputs: {},
             outputs: {},
+            props: properties
+                .filter(([prop]) => !prop.startsWith('_'))
+                .filter(([_prop, descriptor]) => !(descriptor.value instanceof EffectHandle))
+                .map(([prop]) => ({[prop]: prop}))
+                .reduce((l, r) => ({...l, ...r}), {}),
+            state: properties
+                .filter(([prop]) => prop.startsWith('_'))
+                .filter(([_prop, descriptor]) => !(descriptor.value instanceof EffectHandle))
+                .map(([prop]) => ({[prop]: prop}))
+                .reduce((l, r) => ({...l, ...r}), {}),
+            effects: properties
+                .filter(([_prop, descriptor]) => descriptor.value instanceof EffectHandle)
+                .map(([_prop, descriptor]) => (descriptor.value as EffectHandle).stack)
+                .filter((stack): stack is string => Boolean(stack))
+                .map((stack) => parseStack(stack))
+                .filter((effectRef): effectRef is string => Boolean(effectRef)),
             encapsulation: ViewEncapsulation.Emulated,
             changeDetection: ChangeDetectionStrategy.Default,
         };
@@ -41,10 +64,16 @@ export const ng = {
     },
 };
 
-// Forked from Angular internals.
-interface DirectiveMetadata {
+interface DirectiveDebugMetadata {
     inputs: Record<string, string>;
     outputs: Record<string, string>;
+    props: Record<string, string>;
+    state: Record<string, string>;
+    effects: string[];
+}
+
+// Forked from Angular internals.
+interface ComponentDebugMetadata extends DirectiveDebugMetadata {
     encapsulation: ViewEncapsulation;
     changeDetection: ChangeDetectionStrategy;
 }
@@ -59,4 +88,24 @@ enum ViewEncapsulation {
 enum ChangeDetectionStrategy {
     OnPush = 0,
     Default = 1,
+}
+
+/**
+ * Parses the following into the `effect` caller's location URL:
+ *
+ * Error
+ *     at effect (http://localhost:8080/my-comp.js:21:19)
+ *     at <instance_members_initializer> (http://localhost:8080/my-comp.js:5:11)
+ *     at new MyComp (http://localhost:8080/my-comp.js:3:8)
+ *     at http://localhost:8080/my-comp.js:19:16
+ */
+function parseStack(stack: string): string | undefined {
+    const traceLines = stack.matchAll(/^ +at (?<line>.*)$/mg);
+    if (!traceLines) return undefined;
+
+    const caller = Array.from(traceLines).at(1);
+    const match = caller!.groups!['line'].match(/[^(]* \((?<location>[^)]*)\)/);
+    if (!match) return undefined;
+
+    return match.groups!['location'];
 }
